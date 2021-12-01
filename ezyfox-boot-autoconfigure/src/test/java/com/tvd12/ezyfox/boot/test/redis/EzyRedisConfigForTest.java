8d8bd94f717439a5659fe835db2904d0b147ea2d
Dung Ta Van <itprono3@gmail.com>
2021-12-01 20:48:00 +0700

    add redis

---
+++ ezyfox-boot-autoconfigure/src/test/java/com/tvd12/ezyfox/boot/test/redis/EzyRedisConfigForTest.java
@@ -0,0 +1,13 @@
+package com.tvd12.ezyfox.boot.test.redis;
+
+import com.tvd12.ezydata.redis.EzyRedisClientPool;
+import com.tvd12.ezyfox.boot.redis.EzyRedisConfiguration;
+import static org.mockito.Mockito.*;
+
+public class EzyRedisConfigForTest extends EzyRedisConfiguration {
+
+    protected EzyRedisClientPool newClientPool() {
+        EzyRedisClientPool clientPool = mock(EzyRedisClientPool.class);
+        return clientPool;
+    }
+}
---
+++ ezyfox-boot-autoconfigure/src/test/java/com/tvd12/ezyfox/boot/test/redis/EzyRedisConfigTest.java
@@ -0,0 +1,41 @@
+package com.tvd12.ezyfox.boot.test.redis;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.Properties;
+import java.util.Set;
+
+import org.testng.annotations.Test;
+
+import com.tvd12.ezydata.redis.EzyRedisProxy;
+import com.tvd12.ezyfox.bean.EzySingletonFactory;
+import com.tvd12.ezyfox.boot.redis.EzyRedisConfiguration;
+import com.tvd12.test.util.RandomUtil;
+
+public class EzyRedisConfigTest {
+
+    @Test
+    public void autoConfigSuccess() {
+        // given
+        EzySingletonFactory singletonFactory = mock(EzySingletonFactory.class);
+        
+        Set<String> packagesToScan = RandomUtil.randomSet(8, String.class);
+        packagesToScan.add("com.tvd12.ezyfox.boot.test.redis");
+        Properties properties = new Properties();
+        
+        EzyRedisConfiguration sut = new EzyRedisConfigForTest();
+        sut.setPackagesToScan(packagesToScan);
+        sut.setProperties(properties);
+        sut.setSingletonFactory(singletonFactory);
+        
+        // when
+        sut.autoConfig();
+        
+        // then
+        verify(singletonFactory, times(1)).addSingleton(eq("redisProxy"), any(EzyRedisProxy.class));
+    }
+}
